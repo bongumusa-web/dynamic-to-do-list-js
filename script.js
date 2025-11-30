35b6554241761d09949b469529cca4e7d3d67261
Bongumusa Gumede <bonggums@gmail.com>
2025-11-30 15:35:07 +0200

    script.js

--- script.js
+++ script.js
@@ -4,49 +4,70 @@ const addButton = document.getElementById('add-task-btn');
 const taskInput = document.getElementById('task-input');
 const taskList = document.getElementById('task-list');
 
+let tasks = []; // store tasks
 
-function addTask(){
+// load saved tasks
+function loadTaskFromLocalStorage() {
+    const stored = JSON.parse(localStorage.getItem('tasks') || "[]");
+    tasks = stored;
+
+    stored.forEach(taskText => {
+        createListItem(taskText);
+    });
+}
+
+// save tasks
+function saveTasks() {
+    localStorage.setItem("tasks", JSON.stringify(tasks));
+}
+
+// create task element
+function createListItem(taskText) {
+    const listItem = document.createElement('li');
+    const Remove = document.createElement('button');
+    Remove.classList.add('remove-btn');
+
+    listItem.textContent = taskText + " ";
+    Remove.textContent = "Remove";
+    listItem.appendChild(Remove);
+    taskList.appendChild(listItem);
+
+    // remove task
+    Remove.onclick = function () {
+        taskList.removeChild(listItem);
+
+        // remove from array
+        const index = tasks.indexOf(taskText);
+        if (index > -1) {
+            tasks.splice(index, 1);
+            saveTasks();
+        }
+    };
+}
+
+// add task
+function addTask() {
     const taskText = taskInput.value.trim();
 
-    if(taskText === ""){
+    if (taskText === "") {
         alert('Enter a task');
+    } else {
+        tasks.push(taskText); // save to array
+        saveTasks(); // store new array
+        createListItem(taskText); // add to DOM
+        taskInput.value = "";
     }
-    else{
-        const listItem = document.createElement('li');
-        const Remove = document.createElement('button');
-        Remove.classList.add('remove-btn');
+}
 
-        //adding taks text
-        listItem.textContent = taskText + ' ';
-        Remove.textContent = 'Remove';
-        listItem.appendChild(Remove);
-        taskList.appendChild(listItem);
-        taskInput.value = '';
-
-     
-       Remove.onclick = function(){
-            taskList.removeChild(listItem);
-            
-        }
-
-    };
-
-};
-// Add task text to list item when pressing enter key
 taskInput.addEventListener('keypress', function(event){
     if(event.key === 'Enter'){
         addTask();
     }
-
 });
-
-
-
-
-
 
 addButton.addEventListener('click', addTask);
 
-addTask(); // envoke addTask on page load to add initial task
-    
+// load saved tasks on page load
+loadTaskFromLocalStorage();
+
 });
